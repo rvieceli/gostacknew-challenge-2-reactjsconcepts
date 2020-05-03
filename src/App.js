293d73ad12781a081f4c29c7464b34eb48e72060
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2020-05-03 16:30:54 +0200

    list, delete and add new repository with fixed values

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const { data } = await api.get(`/repositories`);
+
+      setRepositories(data);
+    }
+
+    load();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const { data } = await api.post(`/repositories`, {
+        title: "new from front",
+        url: "front.com",
+        techs: ["Restjs", "Nodejs"],
+      });
+
+      setRepositories([data, ...repositories]);
+    } catch {
+      alert("Não foi possivel");
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+      setRepositories(
+        repositories.filter((repository) => repository.id !== id)
+      );
+    } catch {
+      alert("Não foi possivel");
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
